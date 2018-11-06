90a9d4248bdf4211fd858395292ae1d8fdd0f5ec
Luay Adam <luay985@gmail.com>
2018-11-06 18:06:23 +0100

    Adding create new module

--- client/src/components/Modules.jsx
+++ client/src/components/Modules.jsx
@@ -1,32 +1,61 @@
-import React, { Component} from 'react';
-import { getModules } from '../api/modules';
+import React, { Component } from "react";
+import { getModules, createModule } from "../api/modules";
 
 class Modules extends Component {
-  state = {
-    modules: []
-  };
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      modules: []
+    };
+  }
 
   componentDidMount() {
-    getModules().then((modules) => {
-      this.setState({ modules: modules });
+    getModules().then(modules => {
+      this.setState({ modules: modules});
     });
   }
 
+  handleTitleChange = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+  addModule = e => {
+    e.preventDefault();
+    createModule(this.state.title).then(newModule => {
+      this.setState({
+        modules: this.state.modules.concat(newModule),
+        title: ""
+      });
+    });
+  };
+
   render() {
     const { modules } = this.state;
-
-    if (modules.length > 0) {
-      return (
-        <ul>
-          {modules.map((module) => <li key={module._id}>{module.title}</li>)}
-        </ul>
-      )
-    } else {
-      return (
-        <p>There are no modules yet</p>
-      )
+        return (
+          <div>
+            <input
+              className="newTitle"
+              autoFocus
+              type="text"
+              placeholder="Add new Module"
+              onChange={this.handleTitleChange}
+              value={this.state.title}
+            />
+            <button onClick={this.addModule}>Add module</button>
+            {modules.length > 0 ? (
+              <ul>
+                {modules.map(module => (
+                  <li key={module._id}>{module.title}</li>
+                ))}
+              </ul>
+            ) : (
+              <p>There are no modules yet</p>
+            )}
+          </div>
+        );
     }
-  }
 }
 
 export default Modules;
