d93ce7b05612d2a08dce28969b1d88b31053d0d5
Luay Adam <luay985@gmail.com>
2018-11-21 22:44:36 +0100

    adding edit sections for now

--- client/src/components/EditModule.js
+++ client/src/components/EditModule.js
@@ -13,33 +13,74 @@ const editorOptions = {
 };
 
 export default class EditModule extends Component {
+  state = {
+    activeExplanation: true,
+    activeExercise: false,
+    activeEvaluation: false
+  };
+
+  ExplanationChange = () => {
+    this.setState({
+      activeExplanation: true,
+      activeExercise: false,
+      activeEvaluation: false
+    });
+  };
+
+  ExerciseChange = () => {
+    this.setState({
+      activeExplanation: false,
+      activeExercise: true,
+      activeEvaluation: false
+    });
+  };
+  
+  EvaluationChange = () => {
+    this.setState({
+      activeExplanation: false,
+      activeExercise: false,
+      activeEvaluation: true
+    });
+  };
+
   render() {
     const { module, handleChange, handleTitleEditChange, handleContentEdit, handleEdit } = this.props;
     return (
       <div>
+        <div>
         <input
           className="edit-input"
           onChange={handleTitleEditChange}
           defaultValue={module.title}
         />
-        <h5>Explanation content</h5>
-        <ReactQuill
-          modules={editorOptions}
-          onChange={value => handleChange("newExplanation", value)}
-          defaultValue={module.explanation}
-        />
-        <h5>Exercise content</h5>
-        <ReactQuill
-          modules={editorOptions}
-          onChange={value => handleChange("newExercise", value)}
-          defaultValue={module.exercise}
-        />
-        <h5>Evaluation content</h5>
-        <ReactQuill
-          modules={editorOptions}
-          onChange={value => handleChange("newEvaluation", value)}
-          defaultValue={module.evaluation}
-        />
+        <div className={this.state.activeExplanation ? "show-module" : "hide-module"}>
+          <h5>Explanation content</h5>
+          <ReactQuill
+            modules={editorOptions}
+            onChange={value => handleChange("newExplanation", value)}
+            defaultValue={module.explanation}
+          />
+        </div>
+        <div className={this.state.activeExercise ? "show-module" : "hide-module"}>
+          <h5>Exercise content</h5>
+          <ReactQuill
+            modules={editorOptions}
+            onChange={value => handleChange("newExercise", value)}
+            defaultValue={module.exercise}
+          />
+        </div>
+        <div className={this.state.activeEvaluation ? "show-module" : "hide-module"}>
+          <h5>Evaluation content</h5>
+          <ReactQuill
+            modules={editorOptions}
+            onChange={value => handleChange("newEvaluation", value)}
+            defaultValue={module.evaluation}
+          />
+        </div>
+          <button onClick={this.ExplanationChange}>Explanation</button>
+          <button onClick={this.ExerciseChange}>Exercise</button>
+          <button onClick={this.EvaluationChange}>Evaluation</button>
+        </div>
         <button className="update" onClick={() => handleContentEdit(module)}>
           Update
         </button>
