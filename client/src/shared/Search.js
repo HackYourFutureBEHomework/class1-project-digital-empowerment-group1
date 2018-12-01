8af643863e706aa6eceb2b8b840c2e1b243de92c
Luay Adam <luay985@gmail.com>
2018-12-01 07:29:15 +0100

    fixed search, removed hobo button and added more styling

--- client/src/shared/Search.js
+++ client/src/shared/Search.js
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Button } from "reactstrap";
+import { Navbar } from "reactstrap";
 
 export default class Search extends Component {
   render() {
+    const { updateSearch } = this.props;
     return (
       <div>
         <Navbar color="faded" light>
-          <NavbarBrand
-            href="http://www.hobosite.be/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mr-auto">
-            <Button>HOBO VZW</Button>
-          </NavbarBrand>
           <input
             className="searchBar"
             type="text"
-            onChange={this.updateSearch}
+            onChange={updateSearch}
             placeholder="Find a Path..."
           />
         </Navbar>
