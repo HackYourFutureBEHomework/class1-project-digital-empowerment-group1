3f73ad92da2d648fc90d2a086d8e95fa1c03f09c
Luay Adam <luay985@gmail.com>
2018-12-01 11:50:13 +0100

    Added page 404 & other tiny fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Paths from './components/paths/Paths';
 import Modules from './components/modules/Modules';
+import NotFound from './components/404/NotFound';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path=":path(|paths|path|index)" component={Paths} />  
+      <Route exact path="/:path(|paths|path|index)" component={Paths} />  
       <Route path="/paths/:pathId" component={Modules} />
-      <Route render={() => <p>Page not found</p>} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
